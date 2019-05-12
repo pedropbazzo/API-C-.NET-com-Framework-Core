ef5af6a2803666d316de65f9cd68ba4465e19462
pedropbazzo <32115702+pedropbazzo@users.noreply.github.com>
2019-05-12 16:44:41 -0300

    Iniciando API C#.NET

---
+++ README.md
@@ -0,0 +1,16 @@
+# <a href="https://medium.com/@adlerpagliarini" target="_blank">Medium - @adlerpagliarini</a>
+
+# C# .Net Core - Criando uma aplicação utilizando Repository Pattern com dois ORMs diferentes Dapper e Entity Framework Core
+
+# <a href="https://medium.com/@adlerpagliarini/c-net-core-criando-uma-aplica%C3%A7%C3%A3o-utilizando-repository-pattern-com-dois-orms-diferentes-dapper-97e8aa6ca35" target="_blank">Parte 1</a>
+- Modelagem das classes de domínio.
+- Criação do banco de dados com Entity Framework Core e migrations.
+
+# <a href="https://medium.com/@adlerpagliarini/c-net-core-criando-uma-aplica%C3%A7%C3%A3o-utilizando-repository-pattern-com-dois-orms-diferentes-dapper-a821d501e317" target="_blank">Parte 2</a>
+- Repositórios genéricos e específicos com Entity Framework Core.
+- Testes integrados com utilização de transações
+
+# C# .Net Core - Criando uma aplicação utilizando Repository Pattern com dois ORMs diferentes Dapper e Entity Framework Core - Parte 3
+- Repositórios genéricos e específicos com Dapper.
+- Testes integrados com utilização de transações
+
---
+++ UnitTest.Integration.Repositories/Repositories/DataBuilder/TaskToDoBuilder.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTest.Integration.Repositories.Repositories.DataBuilder
+{
+    public class TaskToDoBuilder
+    {
+        private TaskToDo taskToDo;
+        private List<TaskToDo> taskToDoList;
+
+        public TaskToDoBuilder()
+        {
+        }
+
+        public TaskToDo CreateTaskToDo()
+        {
+            taskToDo = new TaskToDo() { Title = "Task from Builder", Start = DateTime.Now, DeadLine = DateTime.Now };
+            return taskToDo;
+        }
+
+        public TaskToDo CreateTaskToDoWithUser(int id)
+        {
+            taskToDo = new TaskToDo() { Title = "Task from Builder", Start = DateTime.Now, DeadLine = DateTime.Now, UserId = id };
+            return taskToDo;
+        }
+
+        public List<TaskToDo> CreateTaskToDoList(int amount)
+        {
+            taskToDoList = new List<TaskToDo>();
+            for (int i = 0; i < amount; i++)
+            {
+                taskToDoList.Add(CreateTaskToDo());
+            }
+
+            return taskToDoList;
+        }
+
+        public List<TaskToDo> CreateTaskToDoListWithUser(int amount, int id)
+        {
+            taskToDoList = new List<TaskToDo>();
+            for (int i = 0; i < amount; i++)
+            {
+                taskToDoList.Add(CreateTaskToDoWithUser(id));
+            }
+
+            return taskToDoList;
+        }
+    }
+}
